2d84b675365d9178c038a8d36af4b2731754a5aa
Matthew Nguyen <matthew.nguyen@me.com>
2025-01-22 21:41:27 -0500

    cleans up the root page

--- app/frontend/routes/root/root.tsx
+++ app/frontend/routes/root/root.tsx
@@ -5,13 +5,15 @@ import { FlowLayout } from '../../reusable-components/flow-layout/flow-layout.ts
 export function Root() {
   return (
     <FlowLayout>
-      <div className="flex justify-center items-center h-screen">
-        <Link
-          to="/signup/account-selection"
-          className="inline-block py-3 px-6 bg-[hsla(244,49%,49%,1)] font-medium text-white rounded-md w-full transition max-w-md"
-        >
-          Get started
-        </Link>
+      <div className="flex justify-center items-start h-screen">
+        <div className="flex flex-col justify-center h-1/6 w-full max-w-md">
+          <Link
+            to="/signup/account-selection"
+            className="inline-block py-3 px-6 bg-[hsla(244,49%,49%,1)] font-medium text-white rounded-md w-full transition"
+          >
+            Get started
+          </Link>
+        </div>
       </div>
     </FlowLayout>
   );
