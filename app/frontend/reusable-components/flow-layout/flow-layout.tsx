d2f8d18b43c0eebaa68934666e96fea8142942c7
Matthew Nguyen <matthew.nguyen@me.com>
2025-01-22 21:34:54 -0500

    tune padding

--- app/frontend/reusable-components/flow-layout/flow-layout.tsx
+++ app/frontend/reusable-components/flow-layout/flow-layout.tsx
@@ -11,7 +11,7 @@ export function FlowLayout({ children }: Props) {
 
   return (
     <div className="h-full pt-3 max-w-[1000px] mx-auto">
-      <div className="w-full flex justify-between pb-4 pt-2">
+      <div className="w-full flex justify-between pb-5 pt-2">
         <div>{user && user.username && <h2>{`Welcome, ${user.username}`}</h2>}</div>
         <div>
           <Link to="/logout" reloadDocument>
